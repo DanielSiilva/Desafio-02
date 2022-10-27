5aa59901512a08312e7a61c30b209afbc33d36de
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-27 00:46:56 -0300

    Feat: se aplicado, esse commit

--- desafio2/src/components/personalData/Styled.js
+++ desafio2/src/components/personalData/Styled.js
@@ -1,36 +1,73 @@
 import styled from "styled-components";
 
-export const MasterBox = styled.div `
-  display: flex;
+export const CaixaPrincipal = styled.div `
+  display:flex;
+  flex-direction: column;
+  justify-content:center;
   align-items: center;
-  flex-direction: column;
-  
-  width: 40vw;
-  height: 70vh;
-  border: 1px solid black;
 
+  button{
+    width:15vw;
+    height: 2rem;
+    cursor:pointer
+  }
 `
 
-export const Form = styled.form `
+
+export const MasterBox = styled.div `
+
   display: flex;
-  flex-direction: column;
-  gap: 1rem;
+  justify-content: center;
+  gap: 2rem;
 
 
 
-
-  label{
-    font-size: 1.5rem;
-  }
-
-
-  input {
-    width: 35vw;
-    height: 3vh;
-
-  }
-
-
+  width: 80vw;
+  height: 80vh;
+  margin-top:10rem;
+  padding: 2rem;
   
 
 `
+export const CaixaPessoa = styled.div`
+    display: flex;
+    align-items:center;
+    flex-direction:column;
+    width: 40vw;
+
+    border: 1px solid black;
+
+    h1{
+      margin-top:50px;
+    }
+
+    form{
+      display: flex;
+      flex-direction:column;
+      width:25rem;
+      margin-top: rem;
+      padding:2rem;
+
+      gap: 1rem;
+    }
+
+    input{
+      height: 2rem;
+    }
+
+
+`
+export const CaixaPaises = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction:column;
+
+    width: 40vw;
+
+    border: 1px solid black;
+
+`
+
+
+
+
--- desafio2/src/hooks/useForm.js
+++ desafio2/src/hooks/useForm.js
@@ -1,16 +1,16 @@
+import { useState } from "react"
 
-import { useState } from "react";
 
-export const useForm = (initialState) =>{
-    const [ form, setForm] = useState(initialState)
+export const useForm = (initialState) => {
+    const [form, setForm] = useState(initialState)
 
     const onChange = (event) =>{
-        const { name, value} = event.target
-        setForm({...form, [name]:value})
+        const { name, value } = event.target 
+        setForm({...form, [name]: value}) 
     }
 
-    const functionClear = () =>{
-        setForm(initialState)
+    const functionClear = () => {
+        setForm(initialState) 
     }
 
     return {form, onChange, functionClear}
